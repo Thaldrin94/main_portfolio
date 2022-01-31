82beda0b3e4b2e5b5d2e8fae2d7cce5031224aa9
Kenny <kenny.supangat@hotmail.com>
2022-01-31 20:51:30 +0900

    first

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Kenny's Portfolio</title>
+    <!-- For Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <!-- For CSS -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <section class="main_content">
+      <div class="intro">
+
+        <div class="intro_word">
+          <p class="title">
+            Hi! I'm Kenny Supangat
+          </p>
+          <p class="my_intro">
+            Currently living in Japan for around 5 years.<br>
+            Besides web design I also code! It's kinda hard though.<br>
+            Wanna take a look at my portfolio ? Click the button bellow.<br>
+            You can see my contact info from the button below too !
+          </p>
+        </div>
+        <div class="buttons">
+          <a href="#">Portfolio</a>
+          <a href="#">Contact Me</a>
+        </div>
+      </div>
+
+      <div class="main_img">
+        <img src="img/main.svg" alt="">
+      </div>
+
+    </section>
+
+    <section class="portfolio_section">
+      <div class="port_wrapper">
+
+        <div class="port_box port_title">
+
+          <div class="port_img_title">
+            <img src="img/port_img.svg" alt="">
+          </div>
+
+          <div class="port_title_word">
+            <p>My Portfolio</p>
+          </div>
+
+        </div>
+
+        <div class="port_box">
+          <div class="port_img">
+            <img src="img/port_1.png" alt="">
+          </div>
+          <div class="port_desc">
+            <p class="comp_name">Jトラスト株式会社</p>
+            <a href="#" class="link">https://www.jt-corp.co.jp/</a>
+            <p>HTML, CSS/SCSS</p>
+          </div>
+        </div>
+
+        <div class="port_box">
+          <div class="port_img">
+            <img src="img/port_1.png" alt="">
+          </div>
+          <div class="port_desc">
+            <p>aaaa</p>
+            <p>bbbb</p>
+            <p>cccc</p>
+          </div>
+        </div>
+
+        <div class="port_box">
+          <div class="port_img">
+            <img src="img/port_1.png" alt="">
+          </div>
+          <div class="port_desc">
+            <p class="comp_name">Jトラスト株式会社</p>
+            <a href="#" class="link">https://www.jt-corp.co.jp/</a>
+            <p>HTML, CSS/SCSS</p>
+          </div>
+        </div>
+
+        <div class="port_box">
+          <div class="port_img">
+            <img src="img/port_1.png" alt="">
+          </div>
+          <div class="port_desc">
+            <p class="comp_name">Jトラスト株式会社</p>
+            <a href="#" class="link">https://www.jt-corp.co.jp/</a>
+            <p>HTML, CSS/SCSS</p>
+          </div>
+        </div>
+
+        <div class="port_box">
+          <div class="port_img">
+            <img src="img/port_1.png" alt="">
+          </div>
+          <div class="port_desc">
+            <p class="comp_name">Jトラスト株式会社</p>
+            <a href="#" class="link">https://www.jt-corp.co.jp/</a>
+            <p>HTML, CSS/SCSS</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+
+    <h1></h1>
+  </body>
+</html>
